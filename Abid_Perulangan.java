903f2ebdd4787c090214790bb9370f5953b88de6
AbidAlHusain <149890564+AbidAlHusain@users.noreply.github.com>
2023-11-04 22:54:09 +0700

    Add files via upload

---
+++ Abid_Perulangan.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package abid;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Abid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                String name = "Abid Al Husain";
+        System.out.println("Nama : " + name);
+        System.out.println("=================");
+        for (int i = 0 ; i<=100 ; i++){
+            if (i>= 10 ){
+                System.out.println(name);
+            }
+            else{
+                System.out.println(i);
+            }    
+        }
+    }
+}
